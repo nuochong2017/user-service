c66fd3673f003dfe0d8b244b54ed86f19d372f4e
zhangxin <insanetomato@me.com>
2018-04-27 22:00:24 +0800

    first commit

---
+++ src/main/java/com/nuochong/platform/userservice/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.nuochong.platform.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
